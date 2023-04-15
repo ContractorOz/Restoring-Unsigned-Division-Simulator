1e76f95483e30f7d37a24275432ba18cad81fbe6
ContractorOz <84410923+ContractorOz@users.noreply.github.com>
2023-04-15 16:38:40 +0800

    shift left using 2d array

--- js/calculate.js
+++ js/calculate.js
@@ -30,10 +30,24 @@ function calculate(){
 		}
 		var neg_m = negate(m)
 
+		var AQ = []
+		AQ.push(temp_a)
+		AQ.push(temp_q)
 		//INITIALIZE STEP
+		// sol.innerHTML = //shift_left(temp_a, temp_q)//test
+		
+		//test onli
+		//var QM = []
+		//QM.push(q.value)
+		//QM.push(m.value)
+		
+		//sol.innerHTML = shift_left(QM)//test
+		AQ = shift_left(AQ)
+		sol.innerHTML = AQ
+		
 		var step=0
-		createBox("a", temp_a, step)
-		createBox("q", temp_q, step)
+		createBox("a", AQ[0], step)
+		createBox("q", AQ[1], step)
 		createBr()
 		
 		//loop for q, //change temp_a and temp_q lang every step
--- js/shl.js
+++ js/shl.js
@@ -1,8 +1,36 @@
-//shifts left entire string of input elem by 1 digit
-function shift_left(elem){
-	var temp = elem
+//shifts left input 2d array by 1 digit (A Q)
+function shift_left(AQ){
+	var temp = ""
+	var str1 = ""
+	var str2 = ""
+	var arr = []
+	//shift left here A and MSB of Q
+	for (i=1; i<AQ[0].length; i++)
+	{
+		//AQ[0][i-1] = AQ[0][i]
+		str1 = str1+AQ[0][i]
+	}
 	
-	//shift left here
+	//get Q MSB to A LSB
+	//AQ[0][AQ[0].length-1] = AQ[1][0]
 	
-	return temp
+	//AQ[0][AQ[0].length-1] = "!"
+	
+	str1= str1+AQ[1][0]
+	arr.push(str1)
+	
+	for (i=1; i<AQ[1].length; i++)
+	{
+		////AQ[0][i-1] = AQ[0][i]
+		str2 = str2+AQ[1][i]
+	}
+	
+	
+	arr.push(str2)
+	// for (i=1; i<AQ[1].length; i++)
+	// {
+		// AQ[1][i-1] = AQ[1][i]
+	// }
+	
+	return arr
 }
